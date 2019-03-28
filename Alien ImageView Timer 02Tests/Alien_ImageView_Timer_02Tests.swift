1ffcceeea1b2d96e2ce8b72f73f8681742fa2121
chanseok <miss3378@naver.com>
2019-03-28 16:11:28 +0900

    Initial Commit

---
+++ Alien ImageView Timer 02Tests/Alien_ImageView_Timer_02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Alien_ImageView_Timer_02Tests.swift
+//  Alien ImageView Timer 02Tests
+//
+//  Created by D7703_21 on 2019. 3. 28..
+//  Copyright © 2019년 D7703_21. All rights reserved.
+//
+
+import XCTest
+@testable import Alien_ImageView_Timer_02
+
+class Alien_ImageView_Timer_02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
